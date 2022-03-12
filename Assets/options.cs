65fd01e472b267c27591c44e8bcf71dd8992557f
A-nn-e <ANMOL2JHA@GMAIL.COM>
2022-03-13 02:02:07 +0530

    Initial Commit

---
+++ Assets/options.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class options : MonoBehaviour
+{
+    public void back()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
